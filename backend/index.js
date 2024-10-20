9441e235c8cf7fab826dcc838d103f783ae8be48
Evan <E0701818@u.nus.edu>
2024-10-20 23:32:38 +0800

    updated schema

--- backend/index.js
+++ backend/index.js
@@ -4,13 +4,12 @@ const mongoose = require('mongoose');
 const app = express();
 const cors = require('cors');
 const port = 3030;
-const { v4: uuidv4 } = require('uuid');
-const Click = require('./models');
+const { Jobs, Departments } = require('./models');
 const path = require('path');
 
 // connect to mongodb
 mongoose.connect(
-  'mongodb+srv://3103:RBm2TluHJL2X8V2B@3103.4rmps.mongodb.net/clicks?retryWrites=true&w=majority&appName=3103/',
+  'mongodb+srv://3103:RBm2TluHJL2X8V2B@3103.4rmps.mongodb.net/mailing?retryWrites=true&w=majority&appName=3103/'
 );
 // mongodb+srv://3103:RBm2TluHJL2X8V2B@3103.4rmps.mongodb.net/?retryWrites=true&w=majority&appName=3103
 
@@ -21,48 +20,61 @@ app.use(cors());
 // get image
 app.use('/images', express.static('images'));
 
-// track click
-app.get('/track', async (req, res) => {
-  const userId = req.query.userId || uuidv4(); // Either get userId from query or generate a new one
-  // const params = JSON.stringify(req.query);     // Convert the query parameters to a string for storage
+// track viewing
+app.get('/image.png', async (req, res) => {
+  const userId = req.query.userId; // Either get userId from query or generate a new one
   const jobId = req.query.jobId;
+
+  console.log('userId:', userId);
+  console.log('jobId:', jobId);
   try {
     // Check if there is already a click from this user with the same params
-    const existingClick = await Click.findOne({ userId, jobId });
+    const user = await Departments.findOne({ email: userId, jobId });
 
-    if (existingClick) {
-      console.log(`User ${userId} has already clicked this link with these parameters.`)
-        
-    } else {
+    if (!user) {
       // Store the click data in MongoDB if no existing record is found
-      const click = new Click({ userId, jobId });
-      await click.save();
+      console.log('error no user found');
+    } else {
+      await Departments.updateOne(
+        { email: userId, jobId },
+        { $set: { hasRead: true } }
+      );
       console.log(`User ${userId} clicked the link with jobId ${jobId}`);
+      // might need to redo this in the future
+      res.sendFile(path.join(__dirname, 'images/download.png'));
     }
-  } catch (err) {
-    console.error(err);
+  } catch (error) {
+    console.error('Failed to track click:', error);
+    res.status(500).send('Failed to track click');
   }
-  // Serve the image immediately after tracking
-  let img = `<img src="http://localhost:3030/images/image1.png"/>`;
-  res.send(img);
 });
 
-
-
-app.get('/stats/all', async (req, res) => {
-    const clicks = await Click.find({}); // return the whole list
-    res.send({
-      clicks: clicks
-    });
+// create jobs (mass email feature)
+app.post('/jobs', async (req, res) => {
+  const { id, departmentCode, totalCount } = req.body;
+  console.log(req.body);
+  date = new Date();
+  const job = new Jobs({ id, departmentCode, date, totalCount });
+  await job.save();
+  res.send(job);
 });
 
-// get clicks for job
-app.get('/stats', async (req, res) => {
-  const clicksForJob = await Click.find({ jobId: req.query.jobId }); 
-  res.send({
-    jobId: req.query.jobId,
-    clicks: clicksForJob.length,
-  });
+// create departments (add try catch later)
+app.post('/departments', async (req, res) => {
+  const { emailList, departmentCode, jobId } = req.body;
+  const departments = await Promise.all(
+    emailList.map(async (email) => {
+      const department = new Departments({
+        email,
+        hasRead: false,
+        departmentCode,
+        jobId,
+      });
+      return department.save(); // Save each department document
+    })
+  );
+
+  res.status(200).send(`Created ${departments.length} departments`);
 });
 
 app.get('/', (req, res) => {
@@ -72,3 +84,22 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+// nic can try later
+// {id, dept, filename, viewcount, total} ->
+// {id : count }
+// app.get('/stats/all', async (req, res) => {
+//   const clicks = await Click.find({}); // return the whole list
+//   res.send({
+//     clicks: clicks,
+//   });
+// });
+
+// get clicks for job
+// app.get('/stats', async (req, res) => {
+//   const clicksForJob = await Click.find({ jobId: req.query.jobId });
+//   res.send({
+//     jobId: req.query.jobId,
+//     clicks: clicksForJob.length,
+//   });
+// });
--- backend/models.js
+++ backend/models.js
@@ -1,10 +1,26 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const ClickSchema = new mongoose.Schema({
-    userId: {type: String, required: true},
-    jobId: {type: String, required: true},
-}, { collection: 'mailing_list'});
+const JobsSchema = new mongoose.Schema(
+  {
+    id: { type: String, required: true, unique: true },
+    departmentCode: { type: String, required: true },
+    date: { type: Date, required: true },
+    totalCount: { type: String, required: true },
+  },
+  { collection: 'jobs' }
+);
 
-const Click = mongoose.model("Click", ClickSchema);
+const DepartmentSchema = new mongoose.Schema(
+  {
+    email: { type: String, required: true },
+    hasRead: { type: Boolean, required: true },
+    departmentCode: { type: String, required: true },
+    jobId: { type: String, ref: 'Job' },
+  },
+  { collection: 'departments' }
+);
 
-module.exports = Click;
+const Jobs = mongoose.model('Job', JobsSchema);
+const Departments = mongoose.model('Department', DepartmentSchema);
+
+module.exports = {Jobs, Departments};
